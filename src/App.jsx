c297f1b49d57a3dee32a2af1695ae9c6492c64d9
Gokul-96 <gokulakrishnan.m96@gmail.com>
2024-09-14 11:45:06 +0530

    Ui setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route , Navigate} from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Profile from './components/Profile';
 import Login from './pages/Login';
+import Register from './pages/Register'; 
 import Navbar from './components/Navbar';
+import { useAuthStore } from './hooks/useAuthStore';
 
 function App() {
+  const { user } = useAuthStore();
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={user ? <Navigate to="/dashboard" />:<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/" />} />
+        <Route path="/profile" element={user ? <Profile /> : <Navigate to="/" />} />
       </Routes>
     </Router>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import { useAuthStore } from '../hooks/useAuthStore';
 import { supabase } from '../services/supabase';
+import { useNavigate, Link } from 'react-router-dom';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { setUser } = useAuthStore();
+  const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -17,16 +19,21 @@ const Login = () => {
       alert(error.message);
     } else {
       setUser(user);
+      navigate('/dashboard'); 
     }
   };
 
   return (
+    <div>
     <form onSubmit={handleLogin}>
       <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
       <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
       <button type="submit">Login</button>
     </form>
-  );
+
+<p>Don't have an account? <Link to="/register">Register here</Link></p>
+</div>  
+);
 };
 
 export default Login;
